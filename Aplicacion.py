30282b19d52a00de3daf9137eacf5956de3ca4ab
Jheremy Ayquipa <jheremy.lol.123@gmail.com>
2023-10-25 09:36:58 -0500

    Parte 1 Ayquipa Jheremy

---
+++ Aplicacion.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 25 09:24:45 2023
+
+@author: ADMIM
+"""
+
+print("\nMenú Opciones")
+print("1. Registrar")
+print("2. Eliminar")
+print("3. Editar")
+print("4. Listar")
+print("5. Salir")
+while True:
+    opcion=input("Seleccione una opcion: ")
+    if opcion =="1":
+        print("Seleccionaste Registar.")
+    elif opcion =="2":
+        print("Seleccionaste Eliminar.")
+    elif opcion=="3":
+        print("Seleccionaste Editar.")
+    elif opcion=="4":
+        print("Seleccionaste Listar.")
+    elif opcion=="5":
+        print("Seleccionaste Salir.")
+        break
+    else:
+        print("Opcion no validad.")
